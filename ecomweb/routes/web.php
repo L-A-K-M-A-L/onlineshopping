8c4dccc13658eaa97bff69e41158118a95af8fb5
Lakmal Prasanna <lakmalprasannakd@gmail.com>
2025-01-18 22:13:12 +0530

    Create route for admin and user dahsboard configure logout button for both users

--- ecomweb/routes/web.php
+++ ecomweb/routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// show user page
+Route::middleware('auth')->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+// show admin page
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
+
