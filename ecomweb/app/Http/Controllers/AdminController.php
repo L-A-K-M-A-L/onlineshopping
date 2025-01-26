838394e4e8ceea34c4af3b99c8345903d7198665
Lakmal Prasanna <lakmalprasannakd@gmail.com>
2025-01-26 22:23:30 +0530

    Create edit  button functioanlity on admin dashboard

--- ecomweb/app/Http/Controllers/AdminController.php
+++ ecomweb/app/Http/Controllers/AdminController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Brand;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Laravel\Facades\Image;
+use Illuminate\Validation\Rule;
 
 class AdminController extends Controller
 {
@@ -15,16 +17,19 @@ class AdminController extends Controller
         return view('admin.index');
     }
 
-    public function brands(){
-        $brands = Brand::orderBy('id','DESC')->paginate(10);
-        return view('admin.brands',compact('brands'));
+    public function brands()
+    {
+        $brands = Brand::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.brands', compact('brands'));
     }
 
-    public function add_brand(){
-       return view('admin.brand-add');
+    public function add_brand()
+    {
+        return view('admin.brand-add');
     }
 
-    public function brand_store(Request $request){
+    public function brand_store(Request $request)
+    {
         $request->validate(
             [
                 'name' => 'required',
@@ -38,23 +43,61 @@ class AdminController extends Controller
         $brand->slug = Str::slug($request->name);
         $image = $request->file('image');
         $file_extension = $request->file('image')->extension();
-        $file_name = Carbon::now()->timestamp.'.'.$file_extension;
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
         $this->GenerateBrandThumbnailsImage($image, $file_name);
         $brand->image = $file_name;
         $brand->save();
 
         return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully');
-
     }
 
-    public function GenerateBrandThumbnailsImage($image, $imageName){
+    public function brand_edit($id)
+    {
+        $brand = Brand::find($id);
+
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request)
+    {
+
+
+        $request->validate([
+            'name' => 'required',
+            'slug' => ['required', Rule::unique('brands', 'slug')->ignore($request->id)],
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('uploads/brands') . '/' . $brand->image)) {
+                File::delete(public_path('uploads/brands') . '/' . $brand->image);
+            }
+
+            $image = $request->file('image');
+            $file_extension = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+            $this->GenerateBrandThumbnailsImage($image, $file_name);
+            $brand->image = $file_name;
+        }
+
+        $brand->save();
+
+        return redirect()->route('admin.brands')->with('status', 'Brand has been Updated successfully');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
 
         $destinationpath = public_path('uploads/brands');
         $img = Image::read($image->path());
 
-        $img->cover(124,124,"top");
-        $img->resize(124, 124, function($constriaint){
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constriaint) {
             $constriaint->aspectRatio();
-        })->save($destinationpath.'/'.$imageName);
+        })->save($destinationpath . '/' . $imageName);
     }
 }
